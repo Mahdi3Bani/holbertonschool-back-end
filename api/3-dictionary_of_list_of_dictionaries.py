50b4b482ea3893c77a68dc9d3f51be527c2e5277
Mahdi3Bani <bani.mahdi666@gmail.com>
2022-12-02 23:17:11 +0100

    api

--- api/3-dictionary_of_list_of_dictionaries.py
+++ api/3-dictionary_of_list_of_dictionaries.py
@@ -13,18 +13,17 @@ if __name__ == '__main__':
         "https://jsonplaceholder.typicode.com/todos",
         params=payload).json()
 
-
     dic = {}
 
     for i in user:
         ident = user.get('id')
         to_do_list = requests.get(
-        "https://jsonplaceholder.typicode.com/todos",
-        params=payload).json()
+            "https://jsonplaceholder.typicode.com/todos",
+            params=payload).json()
         for task in to_do_list:
             dic.update({user.get("id"): [{"task": task.get("task"),
-                                        "completed": task.get("completed"),
-                                        "username": user.get("username")}]})
-    
+                                          "completed": task.get("completed"),
+                                          "username": user.get("username")}]})
+
     with open("todo_all_employees.json", "w") as f:
         json.dump(dic, f)
