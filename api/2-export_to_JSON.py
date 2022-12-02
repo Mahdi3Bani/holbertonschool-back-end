856168e638a25a1d69b29d66c54d43b91c38d57d
Mahdi3Bani <bani.mahdi666@gmail.com>
2022-12-02 23:00:39 +0100

    api

--- api/2-export_to_JSON.py
+++ api/2-export_to_JSON.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
 ''''return something'''
+import json
 import requests
 import sys
 if __name__ == '__main__':
@@ -11,11 +12,11 @@ if __name__ == '__main__':
     to_do_list = requests.get(
         "https://jsonplaceholder.typicode.com/todos",
         params=payload).json()
+    jsonn = {}
+    for task in to_do_list:
 
-    jsonn = {user.get("id"): [{"task": task.get("title"),
-                               "completed": task.get("completed"),
-                               "username": user.get("username")}
-                              for task in to_do_list]}
+        jsonn[user.get("id")] = [{"task": task.get("title"),
+                                        "completed": task.get("completed"),
+                                        "username": user.get("username")}]
     with open(identity + ".json", 'w') as f:
-
-        jsonn.dump(jsonn, f)
+        json.dump(jsonn, f)
