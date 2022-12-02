83a7deb801c03745fd4f810d7f01fb819befbe37
Mahdi3Bani <bani.mahdi666@gmail.com>
2022-12-02 21:49:10 +0100

    api

---
+++ api/0-gather_data_from_an_API.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+''''return something'''
+import requests
+import sys
+if __name__ == '__main__':
+    identity = sys.argv[1]
+    payload = {"userId": identity}
+    user = requests.get(
+        "https://jsonplaceholder.typicode.com/user/{}".format(identity)).json()
+    to_do_list = requests.get(
+        "https://jsonplaceholder.typicode.com/user/todos", params=payload).json()
+
+    l = []
+    for i in to_do_list:
+        if i.get['l']:
+            l.append(i.get('title'))
+    print("Employee {} is done with tasks({}/{}):".format(user.get('name'), len(l),len(to_do_list)))
+    for i in l:
+        print("\t {}".format(l))
