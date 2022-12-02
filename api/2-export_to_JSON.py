ee13a72294433a2e8c1566b54f1a8f6da257d5dc
Mahdi3Bani <bani.mahdi666@gmail.com>
2022-12-02 22:39:36 +0100

    api

--- api/2-export_to_JSON.py
+++ api/2-export_to_JSON.py
@@ -11,12 +11,10 @@ if __name__ == '__main__':
     to_do_list = requests.get(
         "https://jsonplaceholder.typicode.com/todos",
         params=payload).json()
-
-    f = open(str(eval(identity)) + ".csv", "x")
-
+    jsonn = {}
     for task in to_do_list:
-        csv = '"' + str(user.get("id")) + '","' + str(
-            user.get("username")) + '","' + str(
-                task.get("completed")) + '","' + str(
-                    task.get("title")) + '"\n'
-        f.write(csv)
+        jsonn = jsonn + {user.get("id"): [{"task": task.get("title"),
+                                         "completed": task.get("completed"),
+                                         "username": user.get("username")}]}
+    with open(identity + ".json", 'w') as f:
+        jsonn.dump(jsonn,f)
